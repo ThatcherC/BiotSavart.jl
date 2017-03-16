f4ab624123eef07510675b2898b2b242530c0cbf
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2017-03-16 15:47:59 -0400

    Correct some math errors in the source
    dS had an unnecessary t term and dB had a 1/r^2 instead of 1/r^3. dS might still be wrong

--- BiotSavart.jl
+++ BiotSavart.jl
@@ -3,6 +3,8 @@ using Cubature
 μ₀ = 4pi*1e-7 # Tm/A
 I = 1 # A
 
+r = 0.01
+
 immutable Point
   x::Float64
   y::Float64
@@ -14,18 +16,18 @@ immutable Line
    p1::Point
 end
 
-point = Point(0.5,1,0)
+point = Point(0.5,r,0)
 line = Line(Point(0,0,0), Point(1,0,0))
 
 x(t) = (1-t)line.p0.x + t*line.p1.x
 y(t) = (1-t)line.p0.y + t*line.p1.y
 z(t) = (1-t)line.p0.z + t*line.p1.z
 
-r_(t) = [point.x, point.y, point.z] - [x(t), y(t), z(t)]
-ds_(t) = [-t*line.p0.x + t*line.p1.x, -t*line.p0.y + t*line.p1.y, -t*line.p0.z + t*line.p1.z]
+r_(t)  = [point.x, point.y, point.z] - [x(t), y(t), z(t)]
+ds_(t) = [line.p1.x-line.p0.x, line.p1.y-line.p0.y, -line.p1.z-line.p0.z]
 
 function dB(t,v)
-  v[:] = μ₀*I/(4pi)*cross(ds_(t),r_(t))/norm(r_(t))^2
+  v[:] = μ₀*I/(4pi)*cross(ds_(t),r_(t)) / norm(r_(t))^3
 end
 
 # (val, err) = hquadrature(3, (t,v) -> v[:] = μ₀*I/(4pi)*cross(ds_(t),r_(t))/norm(r_(t))^2, 0.0, 1.0, abstol=1e-8)
@@ -33,4 +35,4 @@ end
 
 println(val)
 
-println(μ₀*I/(2pi)*0.5/sqrt(0.5^2 + 1))
+println(μ₀*I/(2pi*r))
