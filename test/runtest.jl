95a083f2cea623da8456451988f532f6210e1746
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2017-03-17 14:47:41 -0400

    Added a test for BfromLines

--- test/runtest.jl
+++ test/runtest.jl
@@ -11,3 +11,21 @@ line = Line(Point(0,0,0), Point(1,0,0))
 i_approx = μ₀/2pi/r
 
 @test abs(val[3]-i_approx)<0.001
+
+
+#Tests for BfromLines
+N=200
+theta = linspace(0,2pi,N)
+x = cos(theta)
+y = sin(theta)
+z = zeros(200)
+
+points = Point.(x,y,z)
+lines = Line.(points[1:N-1],points[2:N])
+lines = vcat(lines, Line(points[N],points[1]))
+
+B = BfromLines(Point(0,0,0),lines,1)
+bCalc = μ₀/2
+println(B)
+println(bCalc)
+@test abs(B[3]-bCalc)<0.00001
